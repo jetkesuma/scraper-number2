2b674eec036f9bc51a825019f81c08e3674aeb45
Kusuma <kusumo999666@gmail.com>
2023-10-04 06:47:12 +0700

    get product

--- super/super/spiders/ts.py
+++ super/super/spiders/ts.py
@@ -10,11 +10,9 @@ class TsSpider(scrapy.Spider):
     start_urls = ["https://www.80stees.com/collections/80s-movies"]
 
     def parse(self, response):
-        detail_products: List[Selector] = response.css ('.hero-body is-flex-column is-justify-center .has-text-weight-bold '
-                                        'is-size-6-mobile is-size-5-tablet heading line-height-1-mobile '
-                                        'has-no-text-transform a')
+        detail_products: List[Selector] = response.css ('.is-grid .grid-item a')
         for detail in detail_products:
-            href = detail.
+            href = detail.attrib.get('href')
             yield response.follow(href, callback=self.parse_detail)
 
 
